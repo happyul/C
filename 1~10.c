cc10454a78bfa652f09f46ce931d038f87891b3a
happyul <108775837+happyul@users.noreply.github.com>
2022-07-20 11:52:31 +0900

    Create 1~10.c

---
+++ 1~10.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//숫자를 입력 하여 1~10까지 출력하기 
+int main()
+{
+	int a;
+	int b;
+	a = 1;
+	scanf("%d",&b);
+	
+	while(a<b)
+	{
+		printf("%d ",a);
+		a = a + 1;
+	}
+
+
+
+return 0;
+}
